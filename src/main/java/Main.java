bb39e70d47cd21c3338b8d935bf2fff1f73f99be
kaloyan <kaloyanknkostov@gmail.com>
2026-05-26 03:45:45 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,52 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         boolean exit = false;
+        Scanner scanner = new Scanner(System.in);
         while (!exit) {
             System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
-            String command = scanner.nextLine();
-            if (command.equals("exit"))
+            String input = scanner.nextLine();
+
+            String words[] = input.split(" ");
+            String command = words[0];
+            String rest[] = Arrays.copyOfRange(words, 1, words.length);
+            String result = String.join(" ", rest);
+
+
+            if (Objects.equals(command,"exit"))
                 exit = true;
-            else if (command.startsWith("echo "))
-                System.out.println(command.substring(5));
-            else if (command.startsWith("type ")) {
-                //echo, exit, and type
-                if (command.substring(5).equals("echo")| command.substring(5).equals("type")|command.substring(5).equals("exit"))
-                    System.out.println(command.substring(5)+" is a shell builtin");
-                else System.out.println(command.substring(5)+": not found");
+            else if (Objects.equals(command,"echo"))
+                System.out.println(result);
+            else if (Objects.equals(command,"type")) {
+
+                System.out.println(type(result));
 
             }
-            else System.out.println(command + ": command not found");
+            //maybe error
+            else System.out.println(input+ ": command not found");
         }
+        scanner.close();
+    }
+
+
+    public static String type(String command){
+       String[] commands = {"exit","echo","type"};
+       String path_commands = System.getenv("PATH");
+       String[] path_command = path_commands.split(":");
+       for(String text:commands){
+            if(Objects.equals(text,command))
+                return command+" is a shell builtin";
+       }
+       for(String path:path_command) {
+           File file = new File(path, command);
+           if (file.exists() && file.canExecute()) {
+               return command + " is " + file.getAbsolutePath();
+           }
+       }
+       return command+": not found";
     }
 }
